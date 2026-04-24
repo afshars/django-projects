0a73d3ed1a6a5779a2c7c5ad5ecaa74681dd38ce
Sara Afshar <73103443+afshars@users.noreply.github.com>
2026-04-25 02:24:57 +0330

    Create serializers.py

---
+++ BookMook/products/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from  .models import Category,Product,File
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ('id','title','description','avatar')
+
+class FileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = File
+        fields = ('id','title','file')
+
+class ProductSerializer(serializers.ModelSerializer):
+    categories = CategorySerializer(many=True)
+    files = FileSerializer(many=True)
+    class Meta:
+        model = Product
+        fields = ('id','title','description','avatar','categories','files')
